ce60f9bc2e0c185e80b1ed5069375b69609f1d8a
Harry <cheuh008@gmail.com>
2024-11-18 14:01:58 +0000

    new file:   MKR_BSEC2_V1/MKR_BSEC2_V1.ino

---
+++ MKR_BSEC2_V1/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials
+// and connection details
+
+#define SECRET_SSID "VodafoneMobileWiFi-594684"  // replace MySSID with your WiFi network name
+#define SECRET_PASS "3470571771"                 // replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 2729600        // replace 0000000 with your channel number
+#define SECRET_W_APIKEY "RY6TNGB58YWHBI2N"  // replace XYZ with your channel write API Key
