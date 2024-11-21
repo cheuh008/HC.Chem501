3a77f52975b21d26856a25acba630f16f3a3d050
Harry <cheuh008@gmail.com>
2024-11-21 22:45:36 +0000

    Changes to be committed: 	modified:   Arduino Code/MKR_BSEC2_ThingSpeak/secrets_template.h 	new file:   Arduino Code/Probs Depracted/MKR_BSEC2_V1/MKR_BSEC2_V1.ino 	new file:   Arduino Code/Probs Depracted/MKR_BSEC2_V1/secrets_template.h 	new file:   Arduino Code/Probs Depracted/MKRtoThingSpeak/MKRtoThingSpeak.ino 	new file:   Arduino Code/Probs Depracted/MKRtoThingSpeak/README.md 	new file:   Arduino Code/Probs Depracted/MKRtoThingSpeak/secrets_template.h

---
+++ Arduino Code/Probs Depracted/MKR_BSEC2_V1/secrets_template.h
@@ -0,0 +1,14 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+#define SECRET_SSID "your_wifi_ssid"  
+#define SECRET_PASS "your_wifi_pw"                 
+
+
+#define SECRET_CH_ID 0000000  
+#define SECRET_API_KEY ""  
+
+//const unsigned long SECRET_CH_IDS[] = {0000000, 0000000, 0000000,0000000};
+//const char* SECRET_API_KEYS[] = {"", "", "", ""};
+
+#endif
