9e4139e7bb894065ffd252e1a28bf6d5beec7cf0
Harry <cheuh008@gmail.com>
2024-11-27 04:45:36 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   Arduino Code/Hs_Old_Code_Might_Work/Ac_EtAc_Classify/Ac_EtAc_Classify.ino
    	new file:   Arduino Code/Hs_Old_Code_Might_Work/Ac_EtAc_Classify1/Ac_EtAc_Classify1.ino
    	new file:   Arduino Code/Hs_Old_Code_Might_Work/BecasueItRandomlyStoppedWorkingAgain/BecasueItRandomlyStoppedWorkingAgain.ino
    	renamed:    Arduino Code/MKR_BSEC2_ThingSpeak/MKR_BSEC2_ThingSpeak.ino -> Arduino Code/Hs_Old_Code_Might_Work/MKR_BSEC2_ThingSpeak/MKR_BSEC2_ThingSpeak.ino
    	renamed:    Arduino Code/MKR_BSEC2_ThingSpeak/README.md -> Arduino Code/Hs_Old_Code_Might_Work/MKR_BSEC2_ThingSpeak/README.md
    	renamed:    Arduino Code/MKR_BSEC2_ThingSpeak/secrets_template.h -> Arduino Code/Hs_Old_Code_Might_Work/MKR_BSEC2_ThingSpeak/secrets_template.h
    	new file:   Arduino Code/Hs_Old_Code_Might_Work/MKR_Slave_Reciever/MKR_Slave_Reciever.ino
    	renamed:    Arduino Code/BecasueItRandomlyStoppedWorkingAgain/BecasueItRandomlyStoppedWorkingAgain.ino -> Arduino Code/Hs_Old_Code_Might_Work/N_V2/N_V2.ino
    	new file:   Arduino Code/Hs_Old_Code_Might_Work/Nicla_Gas_Collector/Nicla_Gas_Collector.ino
    	renamed:    Arduino Code/Nicla_Main_Classifier_Writer/Nicla_Main_Classifier_Writer.ino -> Arduino Code/Hs_Old_Code_Might_Work/Nicla_Main_Classifier_Writer/Nicla_Main_Classifier_Writer.ino
    	new file:   Arduino Code/Hs_Old_Code_Might_Work/Nicla_WORKING_Classifier/Nicla_WORKING_Classifier.ino
    	new file:   Arduino Code/Hs_Old_Code_Might_Work/WriteMultipleFields/WriteMultipleFields.ino
    	new file:   Arduino Code/MKR_Master_Reader_to_ThingSpeak/MKR_Master_Reader_to_ThingSpeak.ino
    	new file:   Arduino Code/MKR_Master_Reader_to_ThingSpeak/README.md
    	new file:   Arduino Code/MKR_Master_Reader_to_ThingSpeak/secrets_template.h
    	new file:   Arduino Code/Nicla_BSEC2_Classifier_Slave_Sender/Nicla_BSEC2_Classifier_Slave_Sender.ino

--- Arduino Code/Hs_Old_Code_Might_Work/MKR_BSEC2_ThingSpeak/MKR_BSEC2_ThingSpeak.ino
+++ Arduino Code/Hs_Old_Code_Might_Work/MKR_BSEC2_ThingSpeak/MKR_BSEC2_ThingSpeak.ino
@@ -1,114 +1,40 @@
-
-// ================================================================================================================================================================================================
-// Note From Authour @cheuh008 @https://github.com/cheuh008/HC.Chem501
-// ================================================================================================================================================================================================
-
-
-// ================================================================================================================================================================================================
-// Libaries
-// ================================================================================================================================================================================================
-
-#include <Wire.h>              // Library for I2C communication with Nicla (needed for BSEC2)
-#include <WiFiNINA.h>          // Library for Wi-Fi connectivity
-#include <WiFiUdp.h>           // Library for UDP communication over Wi-Fi
-#include <RTCZero.h>           // Library for Real Time Clock (RTC) on MKR boards
-#include "secrets.h"           // Include file with sensitive info like SSID, password, ThingSpeak API keys
-#include "ThingSpeak.h"        // ThingSpeak library for cloud data logging
-#include "Arduino_BHY2Host.h"  // Library for BHY2 sensor data from Nicla Vision/BME688
-
-// ================================================================================================================================================================================================
-// iniitlising sesnors global variable
-// ================================================================================================================================================================================================
-
-struct SenosrData {
-  uint16_t iaq;
-  uint16_t iaq_s;
-  float b_voc_eq;
-  uint32_t co2_eq;
-  uint8_t bsec_accu;
-  float comp_t;
-  float comp_h;
-  uint32_t comp_g;
-  float gasVal;
-  float presVal;
-  float tempValue;
-  uint8_t gas0;
-  uint8_t gas1;
-  uint8_t gas2;
-  uint8_t gas3;
-  uint8_t bsec2_accu;
-};
-
-SenosrData sensorData;
+#include <Wire.h>        // Library for I2C communication with Nicla (needed for BSEC2)
+#include <WiFiNINA.h>    // Library for Wi-Fi connectivity
+#include <WiFiUdp.h>     // Library for UDP communication over Wi-Fi
+#include <RTCZero.h>     // Library for Real Time Clock (RTC) on MKR boards
+#include "secrets.h"     // Include file with sensitive info like SSID, password, ThingSpeak API keys
+#include "ThingSpeak.h"  // ThingSpeak library for cloud data logging
 
 WiFiClient client;  // Client to establish Wi-Fi connections
 RTCZero rtc;        // Real-time clock instance for managing date and time
 
 
-// ================================================================================================================================================================================================
-// Inital Setup Function
-// ================================================================================================================================================================================================
-
 void setup() {
-  Serial.begin(115200);
-  while (!Serial) { (1); }
-  BHY2Host.begin();
+  Wire.begin();          // join i2c bus (address optional for master)
+  Serial.begin(115200);  // start serial for output debug
   rtc.begin();
   wifiConnect();
-  Wire.begin();
-  Wire.onReceive(receiveEvent);
 }
 // ================================================================================================================================================================================================
 // Main Function Loop
 // ================================================================================================================================================================================================
 
 void loop() {
-  BHY2Host.update();
-  static auto lastCheck = millis();
   if (WiFi.status() != WL_CONNECTED) wifiConnect();
-  Wire.requestFrom(0x10, sizeof(SensorData))) {
-    Wire.readBytes((char*)&sensorData, sizeof(SensorData));
-    sendData(0);
-    sendData(1);
+  Wire.requestFrom(2, 6);   // request 6 bytes from slave device #2
+  while (Wire.available())  // slave may send less than requested
+  {
+    char c = Wire.read();  // receive a byte as character
+    Serial.print(c);       // print the character
   }
 }
 
-void receiveEvent(size_t howMany) {
-  int receivedValue = Wire.read(); // Read received value from Master
 
-  Serial.print("Received value: ");
-  Serial.println(receivedValue);
-
-  digitalWrite(ledPin, receivedValue % 2); // Toggle LED based on received value
-}
-
-// ================================================================================================================================================================================================
-// Function to send data to ThingSpeak client
-// ================================================================================================================================================================================================
-
-void sendData(int batch) {
-  int startField = batch * 8;
-  String msg;
-  float* dataPtr = (float*)&receivedData;
-  for (int i = 0; i < 8; i++) {
-    int fieldIndex = startField + i;
-    if (fieldIndex >= (sizeof(SensorData) / sizeof(float))) break;  // Prevent overflow
-    ThingSpeak.setField(i + 1, dataPtr[fieldIndex]);                // Set ThingSpeak field
-  }
-  msg = (batch == 0) ? "BSEC data updated at: " + printTime() : "Gas data updated at: " + printTime() ThingSpeak.setStatus(msg);
-  int result = ThingSpeak.writeFields(channelIDs[batch + 2], APIKeys[batch + 2]);
-  if (result == 200) {
-    Serial.println("Channel " ++String(batch + 2) + " update successful: ");
-  } else {
-    Serial.println("HTTP error code: " + String(result));
-  }
-}
 // ================================================================================================================================================================================================
 // Function to connect to Wi-Fi and initialize the ThingSpeak client
 // ================================================================================================================================================================================================
 
 void wifiConnect() {
-
   Serial.println("Connecting to WiFi...");  // Debug message indicating Wi-Fi connection attempt
   while (WiFi.status() != WL_CONNECTED) {   // Wait for Wi-Fi connection to be established
     WiFi.begin(SECRET_SSID, SECRET_PASS);   // Initialize Wi-Fi using the provided SSID and password
