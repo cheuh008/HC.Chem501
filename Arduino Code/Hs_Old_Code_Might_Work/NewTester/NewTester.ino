f90fad675f165b460d85c7bfaf625312acfa3212
Harry <cheuh008@gmail.com>
2024-11-28 15:48:14 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is behind 'origin/main' by 16 commits, and can be fast-forwarded.
      (use "git pull" to update your local branch)
    
    Changes to be committed:
    	modified:   Arduino Code/Hs_Old_Code_Might_Work/NewTester/NewTester.ino
    
    Changes not staged for commit:
    	modified:   Arduino Code/Hs_Old_Code_Might_Work/N_V2/N_V2.ino
    	modified:   Arduino Code/Hs_Old_Code_Might_Work/NewTester/NewTester.ino
    	modified:   Nicla_BME2V2.bmeproject/project.db
    	modified:   Nicla_BME2V2.bmeproject/project.db-shm
    	modified:   Nicla_BME2V2.bmeproject/project.db-wal
    
    Untracked files:
    	Arduino Code/Hs_Old_Code_Might_Work/N_V2/BSEC2CONFIG.h

--- Arduino Code/Hs_Old_Code_Might_Work/NewTester/NewTester.ino
+++ Arduino Code/Hs_Old_Code_Might_Work/NewTester/NewTester.ino
@@ -1,9 +1,3 @@
-/* 
- * This sketch shows how nicla can be used in standalone mode.
- * Without the need for an host, nicla can run sketches that 
- * are able to configure the bhi sensors and are able to read all 
- * the bhi sensors data.
-*/
 
 #include "Arduino.h"
 #include "Arduino_BHY2.h"
@@ -13,6 +7,7 @@ SensorXYZ gyro(SENSOR_ID_GYRO);
 Sensor temp(SENSOR_ID_TEMP);
 Sensor gas(SENSOR_ID_GAS);
 SensorQuaternion rotation(SENSOR_ID_RV);
+//SensorBSEC bsec(SENSOR_ID_BSEC);
 SensorBSEC2 bsec2(SENSOR_ID_BSEC2);
 
 const uint8_t BSEC2CONFIG[1943] =
@@ -23,65 +18,28 @@ const uint8_t BSEC2CONFIG[1943] =
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial)
-    ;
-
   BHY2.begin();
-
   accel.begin();
   gyro.begin();
   temp.begin();
   gas.begin();
   rotation.begin();
-
+  //bsec.begin();
   sensortec.bhy2_bsec2_setConfigString(BSEC2CONFIG, sizeof(BSEC2CONFIG) / sizeof(BSEC2CONFIG[0]));
   bsec2.begin();
-
-  SensorConfig cfg = accel.getConfiguration();
-  Serial.println(String("range of accel: +/-") + cfg.range + String("g"));
-  accel.setRange(2);  //this sets the range of accel to +/-4g,
-  cfg = accel.getConfiguration();
-  Serial.println(String("range of accel: +/-") + cfg.range + String("g"));
-
-  cfg = gyro.getConfiguration();
-  Serial.println(String("range of gyro: +/-") + cfg.range + String("dps"));
-  gyro.setRange(1000);
-  cfg = gyro.getConfiguration();
-  Serial.println(String("range of gyro: +/-") + cfg.range + String("dps"));
 }
 
 void loop() {
-  static auto printTime = millis();
-
-  // Update function should be continuously polled
   BHY2.update();
-
+  static auto printTime = millis();
   if (millis() - printTime >= 1000) {
     printTime = millis();
-
-    if (accel.dataAvailable()) {
-      Serial.println(String("acceleration: ") + accel.toString());
-      accel.clearDataAvailFlag();
-    }
-
-    if (gyro.dataAvailable()) {
-      Serial.println(String("gyroscope: ") + gyro.toString());
-      gyro.clearDataAvailFlag();
-    }
-
-    if (temp.dataAvailable()) {
-      Serial.println(String("temperature: ") + String(temp.value(), 3));
-      temp.clearDataAvailFlag();
-    }
-
-    if (gas.dataAvailable()) {
-      Serial.println(String("gas: ") + String(gas.value(), 3));
-      gas.clearDataAvailFlag();
-    }
-
-    if (rotation.dataAvailable()) {
-      Serial.println(String("rotation: ") + rotation.toString());
-      rotation.clearDataAvailFlag();
-    }
+    Serial.print(String("acceleration: ") + accel.toString());
+    Serial.print(String("gyroscope: ") + gyro.toString());
+    Serial.println(String("temperature: ") + String(temp.value(), 3));
+    Serial.println(String("gas: ") + String(gas.value(), 3));
+    Serial.print(String("rotation: ") + rotation.toString());
+    //Serial.print(bsec.toString());
+    Serial.println(bsec2.toString());
   }
 }
