221715f4eb62f90ef033500add9d2785bb4f5956
Harry <cheuh008@gmail.com>
2024-12-06 00:31:27 +0000

    updates to consolidated code

--- Arduino Code/Consolidated/Smoke_MKR/Smoke_MKR.ino
+++ Arduino Code/Consolidated/Smoke_MKR/Smoke_MKR.ino
@@ -1,58 +1,127 @@
-#include <Wire.h>        // Library for I2C communication with Nicla (needed for BSEC2)
-#include <WiFiNINA.h>    // Library for Wi-Fi connectivity
-#include <WiFiUdp.h>     // Library for UDP communication over Wi-Fi
-#include <RTCZero.h>     // Library for Real Time Clock (RTC) on MKR boards
-#include "secrets.h"     // Include file with sensitive info like SSID, password, ThingSpeak API keys
-#include "ThingSpeak.h"  // ThingSpeak library for cloud data logging
+
+#include <Wire.h>              // Library for I2C communication with Nicla (needed for BSEC2)
+#include <WiFiNINA.h>          // Library for Wi-Fi connectivity
+#include <WiFiUdp.h>           // Library for UDP communication over Wi-Fi
+#include <RTCZero.h>           // Library for Real Time Clock (RTC) on MKR boards
+#include "Arduino.h"           //
+#include "functions.h"         //
+#include "secrets.h"           // Include file with sensitive info like SSID, password, ThingSpeak API keys
+#include "ThingSpeak.h"        // ThingSpeak library for cloud data logging
 #include "Arduino_BHY2Host.h"  // Bosch Host library for managing sensor data
+
+Sensor gas(SENSOR_ID_GAS);        // Gas sensor instance using the GAS sensor ID
+Sensor pres(SENSOR_ID_BARO);      // Pressure sensor instance using the BARO sensor ID
+SensorBSEC bsec(SENSOR_ID_BSEC);  // Bosch BSEC sensor instance for air quality data
 
 WiFiClient client;  // Client to establish Wi-Fi connections
 RTCZero rtc;        // Real-time clock instance for managing date and time
 
-int size = 5;
-uint8_t receivedData[5];
+int mode = 2;  // Default mode: 0 for Standalone, 1 for Classifier, 2 for Collector
 
 void setup() {
-  Wire.begin();          // join i2c bus (address optional for master)
   Serial.begin(115200);  // start serial for output debug
+  BHY2Host.begin();      //
+  conditional();
   rtc.begin();
   wifiConnect();
 }
 
 void loop() {
-  if (WiFi.status() != WL_CONNECTED) wifiConnect();
-
+  if (mode == 0) BHY2Host.update();
   static unsigned long lastCheck = millis();
-
-  if (millis() - lastCheck >= 2000) {
+  if (WiFi.status() != WL_CONNECTED) wifiConnect();
+  if (Serial.available() > 0) {      // Check if data is available on the serial port
+    int input = Serial.parseInt();   // Read integer input
+    Serial.read();                   // Clear
+    if (input >= 0 && input <= 2) {  // Validate the mode
+      Wire.beginTransmission(2);     // Begin transmission to slave with address 2
+      Wire.write(input);             //
+      Wire.endTransmission();        // End transmission
+      mode = input;                  // Update Mode
+      conditional();                 // Init Sensors
+    }
+  }
+  if (millis() - lastCheck >= 1000) {
     lastCheck = millis();
+    getData();
+  }
+}
 
-    Wire.requestFrom(2, size);        // Request 5 bytes from slave device #2
-    Serial.print("Received data: ");  // Print received data
+void conditional() {  //
+  if (mode == 0) {    //
+    gas.begin();      // Initialize the gas sensor
+    pres.begin();     // Initialize the pressure sensor
+    bsec.begin();     // Initialize the Bosch BSEC sensor
+    Wire.end();       //
+  } else if (mode == 1 || mode == 2) {
+    gas.end();     // Initialize the gas sensor
+    pres.end();    // Initialize the pressure sensor
+    bsec.end();    // Initialize the Bosch BSEC sensor
+    Wire.begin();  //
+  }
+}
 
-    if (Wire.available() == size) {  // Ensure 5 bytes are received
-      for (int i = 0; i < size; i++) {
-        receivedData[i] = Wire.read();
-        ThingSpeak.setField(i + 1, receivedData[i]);
-        Serial.print(receivedData[i]);
-        Serial.print(" ");
-      }
-      Serial.println();
+void getData() {
+  if (mode == 0) {
+    String sensorData[8] = {
+      String(gas.value()),      // Get the gas sensor value as comp_g is 0 due to oversized byte in liobary (can change to 30 to accomadate)
+      String(pres.value()),     // Get the humidity data as a value
+      String(bsec.iaq_s()),     // Get the temperature sensor value
+      String(bsec.b_voc_eq()),  // Get the pressure sensor value
+      String(bsec.co2_eq()),    // Get the IAQ (Indoor Air Quality) value from BSEC
+      String(bsec.comp_t()),    // Get the VOC equivalent value from BSEC
+      String(bsec.comp_h()),    // Get the CO2 equivalent value from BSEC
+      String(bsec.accuracy())   // Get the accuracy level from BSEC
+    };
+    int size = 8;
+    String VOCmsg = "VOC values updated at: " + printTime();            // Construct the status message with the current timestamp
+    sendData(sensorData, size, VOCmsg, ChannelIDs[3], APIKeys[3]);      //
+  } else if (mode == 1) {                                               //
+    int size = 5;                                                       //
+    String receivedData[5];                                             //
+    Wire.requestFrom(2, size);                                          // Request 5 bytes from slave device #2
+    Serial.print("Received data: ");                                    // Print received data
+    if (Wire.available() == size) {                                     // Ensure 5 bytes are received
+      for (int i = 0; i < size; i++) {                                  //
+        receivedData[i] = String(Wire.read());                          //
+        Serial.print(receivedData[i]);                                  //
+      }                                                                 //
+    }                                                                   //
+    String BSEC2msg = "BSEC2 values updated at: " + printTime();        // Construct the status message with the current timestamp
+    sendData(receivedData, size, BSEC2msg, ChannelIDs[2], APIKeys[2]);  //
+  } else if (mode == 2) {                                               //
+    int size = 6;
+    String data = "";
+    while (Wire.available()) {
+      char c = Wire.read();  // Read each character from the I2C buffer
+      data += c;
     }
-    else {
-      Serial.println("Error: Received less than expected bytes!");
-      while (Wire.available()) Wire.read();                     // Clear buffer
-    }                                                           // Newline after printing all data
-    String msg = "Values updated at: " + printTime();           // Construct the status message with the current timestamp
-    ThingSpeak.setStatus(msg);                                  // Set the ThingSpeak channel status with the timestamp message
-    Serial.println(msg);                                        // Print the status message to the Serial Monitor
-    int x = ThingSpeak.writeFields(ChannelIDs[2], APIKeys[2]);  // Write the fields to ThingSpeak
-    if (x == 200) {                                             // If the update is successful
-      Serial.println("Channel update successful.");             // Print success message
+    int index = 0;
+    String fields[6];
+    for (int i = 0; i < size; i++) {
+      index = data.indexOf(' ');
+      fields[i] = data.substring(0, index);
+      data.remove(0, index + 1);
     }
-    else {                                                    // If there was an error with the HTTP request
-      Serial.println(" HTTP error code " + String(x));          // Print HTTP error code to help debug
-    }
+    String collectormsg = "Gas values updated at: " + printTime();
+    sendData(fields, size, collectormsg, ChannelIDs[1], APIKeys[1]);
+  }
+}
+
+void sendData(String data[], int size, String msg, unsigned long channelId, const char* ApiKey) {
+  for (int i = 0; i < size; i++) {        // Send data to ThingSpeak and print to Serial for debugging
+    ThingSpeak.setField(i + 1, data[i]);  // Set the appropriate field on ThingSpeak (Starts at 1)
+    Serial.print(data[i]);                // Print sensor value to Serial
+    Serial.print(" ");                    // Add space between values for readability
+  }
+  Serial.println();                                   // Newline after printing all sensor values
+  ThingSpeak.setStatus(msg);                          // Set the ThingSpeak channel status with the timestamp message
+  int x = ThingSpeak.writeFields(channelId, ApiKey);  // Write the fields to ThingSpeak
+  if (x == 200) {                                     // If the update is successful
+    Serial.println("Channel update successful.");     // Print success message
+    Serial.println(msg);                              // Print the status message to the Serial Monitor
+  } else {                                            // If there was an error with the HTTP request
+    Serial.println(" HTTP error code " + String(x));  // Print HTTP error code to help debug
   }
 }
 
@@ -77,8 +146,7 @@ void getTime() {
   if (numberOfTries == maxTries) {                       // If the maximum retries are reached
     Serial.print("NTP unreachable!!");                   // Print error message
     wifiConnect();                                       //
-  }
-  else {
+  } else {
     Serial.print("Epoch received: ");  // Print the fetched epoch time
     Serial.println(epoch);             // Print the actual epoch value
     rtc.setEpoch(epoch);               // Set the RTC with the fetched epoch time
